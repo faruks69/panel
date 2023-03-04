c4f44fab18a16f90262d069e089ac809bdc3314e
faruks69 <71905990+faruks69@users.noreply.github.com>
2023-03-04 23:16:44 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'tools');
-define('DB_PASSWORD', 'x@XaDf4bdQeekka');
-define('DB_NAME', 'tools');
+define('DB_SERVER', 'niscambodia.com');
+define('DB_USERNAME', 'panel');
+define('DB_PASSWORD', 'GyocgEktH3YjkdMa');
+define('DB_NAME', 'panel');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
